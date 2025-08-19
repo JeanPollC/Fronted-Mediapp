529de19994380e8e63f0c2f12df89ae99846eadf
Jean Poll Carrera <jeanpollcarrerazarate@gmail.com>
2025-08-19 18:01:02 -0500

    update

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,7 +6,9 @@ import { MatMenuModule} from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MenuService } from '../../services/menu.service';
+import { Menu } from '../../model/menu';
 
 @Component({
   selector: 'app-layout',
@@ -19,11 +21,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    RouterLinkActive
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+
+  menus: Menu[];
+
+  private menuService = inject(MenuService);
+
+  ngOnInit(): void {
+    this.menuService.getMenuChanges().subscribe(data => this.menus = data);
+  }
 
 }
---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { GenericService } from './generic.service';
+import { HttpClient } from '@angular/common/http';
+import { Menu } from '../model/menu';
+import { environment } from '../../environments/environment.development';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService extends GenericService<Menu> {
+
+  private menuChange = new Subject<Menu[]>();
+
+  constructor() {
+    super(
+      inject(HttpClient),
+      `${environment.HOST}/menus`
+    );
+   }
+
+   getMenusByUser(){
+    return this.http.post<Menu[]>(`${environment.HOST}/menus/user`, {});
+   }
+
+   getMenuChanges(){
+    return this.menuChange.asObservable();
+   }
+
+   serMenuChangue(menus: Menu[]){
+    this.menuChange.next(menus);
+   }
+}
